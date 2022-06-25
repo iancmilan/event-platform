b3e62fad385dc3854c07b72ea6517a0c175a96a1
ian <iancmilan@unochapeco.edu.br>
2022-06-25 01:39:59 -0300

    inscrição via graphql

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 export const client = new ApolloClient({
   uri: 'https://api-sa-east-1.graphcms.com/v2/cl4qfdsvf3nci01xnazpea32b/master',
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
